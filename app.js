0e3f0a33829aef567ac2224fb7b41033f076aafe
vasimmhanif <vasimjikani.com>
2021-04-20 12:56:40 +0530

    app changes reverted.

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/jack', booksRouter);
+app.use('/books', booksRouter);
 
 module.exports = app;
