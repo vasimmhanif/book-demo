b1585193bbb039a407251232b19874b0c53a54b4
vasimmhanif <vasimjikani.com>
2021-05-04 20:36:13 +0530

    publishedDate formatted.

--- public/js/book-list.js
+++ public/js/book-list.js
@@ -6,33 +6,47 @@ function loadBookList(bookList) {
         bookHtml = "No Books Found";
     } else {
         bookList.forEach(function(bookItem, index) {
-            bookHtml += `<div class="book-item">   
-                            <div class="item">
-                                <span class="title">ISBN : </span>
-                                <span class="value">${bookItem.isbn}</span>
-                            </div>
+            var publishedDateObject = new Date(bookItem.publishedDate);
+            var publishedMonthStr = publishedDateObject.getMonth() + 1;
+            if( publishedMonthStr < 10 ) {
+                publishedMonthStr = "0" + publishedMonthStr;
+            }
 
-                            <div class="item">
-                                <span class="title">Title : </span>
-                                <span class="value">${bookItem.title}</span>
-                            </div>    
+            var publishedDateStr = publishedDateObject.getDate();
+            if( publishedDateStr < 10 ) {
+                publishedDateStr = "0" + publishedDateStr;
+            }
+
+            var publishedFormattedDate = publishedDateStr + "/" + publishedMonthStr + "/" + publishedDateObject.getFullYear();
+            
+            bookHtml += "<div class=\"book-item\">" +
                             
-                            <div class="item">
-                                <span class="title">Author : </span>
-                                <span class="value">${bookItem.author}</span>
-                            </div>
+                            "<div class=\"item\">" +
+                                "<span class=\"title\">ISBN : </span>" +
+                                "<span class=\"value1\">" + bookItem.isbn + "</span>" +
+                            "</div>" +
+
+                            "<div class=\"item\">" +
+                                "<span class=\"title\">Title : </span>" +
+                                "<span class=\"value\">" + bookItem.title + "</span>" +
+                            "</div>" +   
                             
-                            <div class="item">
-                                <span class="title">Publisher : </span>
-                                <span class="value">${bookItem.publisher}</span>
-                            </div>
+                            "<div class=\"item\">" +
+                                "<span class=\"title\">Author : </span>" +
+                                "<span class=\"value\">" + bookItem.author + "</span>" +
+                            "</div>" +
+                            
+                            "<div class=\"item\">" +
+                                "<span class=\"title\">Publisher : </span>" +
+                                "<span class=\"value\">" + bookItem.publisher + "</span>" +
+                            "</div>" +
 
-                            <div class="item">
-                                <span class="title">Published Date : </span>
-                                <span class="value">${bookItem.publishedDate}</span>
-                            </div>
+                            "<div class=\"item\">" +
+                                "<span class=\"title\">Published Date : </span>" +
+                                "<span class=\"value\">" + publishedFormattedDate + "</span>" +
+                            "</div>" +
 
-                        </div>`;
+                        "</div>"
         });
     }
     document.getElementById("bookListContainer").innerHTML = bookHtml; 
