7e2c03bd3d652316ed42070c93de96fc8e8bf813
vasimmhanif <vasimjikani.com>
2021-07-05 23:51:23 +0530

    database details implimented to local.

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
     db : {
-        host: env.DB_HOST || 'freedb.tech',
-        user: env.DB_USER || 'freedbtech_vasimmhanif',
-        password: env.DB_PASSWORD || 'jack',
-        database: env.DB_NAME || 'freedbtech_vasimdb'
+        host: env.DB_HOST || 'localhost',
+        user: env.DB_USER || 'root',
+        password: env.DB_PASSWORD || 'root',
+        database: env.DB_NAME || 'learning_nodejs'
     } , 
     listPerPage: env.LIST_PER_PAGE || 10
 };
